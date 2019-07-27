265d2d7b3994685f8f34994210dad8f47170a098
Vicluar Daniel Merchan Briceno <24820@norwegian.com>
2019-07-27 20:54:46 +0200

    Old Solution: Setting up the Values controller to use the Old Solution classes.

--- Specification_Pattern/Specification_Pattern/Controllers/ValuesController.cs
+++ Specification_Pattern/Specification_Pattern/Controllers/ValuesController.cs
@@ -3,43 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Specification_Pattern.Old_Solution;
 
 namespace Specification_Pattern.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ValuesController : ControllerBase
+    [Route("api/v1/[controller]")]
+    public class ValuesController : Controller
     {
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public List<CourseDTO> GetCourses(string cultureCode)
         {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            var courses = DataTransferObjectsHelper.GetCourseItems(cultureCode);
+            return courses;
         }
     }
 }
